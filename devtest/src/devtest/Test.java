6b4739724962b762ccddce0bc63fd3c0ae58139c
Julia <Julia@DESKTOP-3LUJ1TN>
2023-10-18 12:40:35 +0900

    wh_메소드 수정 , 메소드 추가

--- devtest/src/devtest/Test.java
+++ devtest/src/devtest/Test.java
@@ -1,9 +1,13 @@
 package devtest;
 
 public class Test {
+	public void inputName() {
+		System.out.println("하이");
+	}
 	public static void main(String[] args) {
-		System.out.println("신기하다");
-		System.out.println("나도 정말 신기해!!");
-		System.out.println("나도 신기하다~~~~~");
+		System.out.println("우현이짱 ");
+		System.out.println("우리조 개짱 ");
+		System.out.println("!!!!!!!~");
+	
 	}
 }
