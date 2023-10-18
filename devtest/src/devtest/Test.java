14f3c7f6e6c303ea0b1f00da47727184c4bf8248
Julia <Julia@DESKTOP-3LUJ1TN>
2023-10-18 14:24:37 +0900

    wh name메소드만들었음

--- devtest/src/devtest/Test.java
+++ devtest/src/devtest/Test.java
@@ -6,8 +6,11 @@ public class Test {
 	}
 	public static void main(String[] args) {
 		System.out.println("화이 ");
-		System.out.println("우리조 개짱 ");
+		System.out.println("우리조 개짱!!!!! ");
 		
 	
+	}
+	public void name() {
+		System.out.println("하이");
 	}
 }
